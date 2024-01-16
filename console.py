d918aaba589e15ec49a9fe110d2ce2d9a61b9a03
Christian Wamalwa <bobitlmr188221@spu.ac.ke>
2024-01-16 14:09:17 +0300

    Refactor the do_create function

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -114,17 +114,61 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, args):
-        """ Create an object of any class"""
+        """Create an object with given parameters"""
         if not args:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+
+        parts = args.split()
+
+        class_name = parts[0]
+
+        if class_name not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
+
+        parts = parts[1:]
+
+        params = {}
+
+        for part in parts:
+            key_value = part.split('=')
+
+            if len(key_value) != 2:
+                print("** invalid parameter format: {} **".format(part))
+                return
+
+            key, raw_value = key_value
+            value = None
+
+            # Parse the value based on the syntax
+            if raw_value.startswith('"') and raw_value.endswith('"'):
+                # String: remove double quotes and replace underscores with spaces
+                value = raw_value[1:-1].replace('_', ' ')
+            elif '.' in raw_value:
+                # Float
+                try:
+                    value = float(raw_value)
+                except ValueError:
+                    print("** invalid float value: {} **".format(raw_value))
+                    return
+            else:
+                # Integer: default case
+                try:
+                    value = int(raw_value)
+                except ValueError:
+                    print("** invalid integer value: {} **".format(raw_value))
+                    return
+
+            # Add the key-value pair to the dictionary
+            params[key] = value
+
+        # Create a new instance with the parameters
+        new_instance = HBNBCommand.classes[class_name](**params)
         storage.save()
         print(new_instance.id)
         storage.save()
+
 
     def help_create(self):
         """ Help information for the create method """
@@ -272,7 +316,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -280,10 +324,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
